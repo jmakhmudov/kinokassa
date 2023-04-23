ba4fcf4ebbef950743158a03620d1092e9012132
JALOLIDDIN MAKHMUDOV <makhmudov@tuta.io>
2023-04-23 15:27:52 +0500

    pages cinema and premieres

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,13 @@
 import styles from '@/styles/Section.module.css'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export default function Section({ title, data, cinemas }) {
+    const router = useRouter()
 
+    const handleClick = (id, cinema) => {
+        cinema ? router.push(`/cinemas/${id}`) :router.push(`/premieres/${id}`)
+    }
 
     return (
         <section className={styles.sectionBox}>
@@ -15,7 +20,7 @@ export default function Section({ title, data, cinemas }) {
                     data.map((card) => {
                         return (
                             cinemas ?
-                                <div key={card.id} className={styles.cardBox}>
+                                <div onClick={() => handleClick(card.id, true)} key={card.id} className={styles.cardBox}>
                                     <div className={styles.cardImg}>
                                         <Image src={card.logo_url} layout='fill' />
                                     </div>
@@ -27,7 +32,7 @@ export default function Section({ title, data, cinemas }) {
                                     </section>
                                 </div>
                                 :
-                                <div key={card.id} className={styles.cardBox}>
+                                <div key={card.id} onClick={() => handleClick(card.id, false)} className={styles.cardBox}>
                                     <div className={styles.cardImg}>
                                         <Image src={card.preview} layout='fill' />
                                     </div>
